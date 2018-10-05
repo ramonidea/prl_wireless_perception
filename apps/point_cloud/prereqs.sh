b9dcadd32eda8219251d3f58e0abb019b5d29c38
Ramon Qu <quramon@gmail.com>
2018-10-05 14:00:45 -0700

    Update prereqs.sh
    update the links

--- apps/point_cloud/prereqs.sh
+++ apps/point_cloud/prereqs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir point_cloud && cd point_cloud
+mkdir ../../../point_cloud && cd ../../../point_cloud
 wstool init src
 catkin init
 catkin config --extend /opt/ros/kinetic
 cd src
-git clone https://github.com/intel-ros/realsense.git
-cp -a ../../image_common ./
-cp -a ../../immage_geometry ./
-cp -a ../../image_pipeline ./
-cp -a ../../image_transport_plugins ./
+git clone https://github.com/intel-ros/realsense
+git clone https://github.com/ros-perception/image_common
+https://github.com/ros-perception/vision_opencv
+https://github.com/ros-perception/image_pipeline
+https://github.com/ros-perception/image_transport_plugins
 cd ../
-catkin build
+catkin_make install
