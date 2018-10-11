d42a0c37782a8e527c65e80b54f0dc10ee133df7
Ramon Qu <quramon@gmail.com>
2018-10-11 13:53:48 -0700

    Update install_fingervision.sh

--- RealSense_Install/install_fingervision.sh
+++ RealSense_Install/install_fingervision.sh
@@ -3,14 +3,17 @@ sudo apt-get -f -y install git g++ make cmake automake libtool pkg-config
 sudo apt-get -f -y install libboost-all-dev libboost-dev
 sudo apt-get -f -y install python-setuptools python python-numpy python-scipy python-sklearn python-matplotlib python-tk
 sudo apt-get -f -y install libv4l-0 libv4l-dev
+
+#Install OpenCV
 cd ~/.local/
 git clone --branch 2.4 --depth 1 https://github.com/Itseez/opencv.git
 cd opencv
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$HOME/.local ..
-make -j8
+make -j4
 make install
+# Install ros packages
 sudo apt-get -f install ros-kinetic-moveit-commander ros-kinetic-moveit-planners ros-kinetic-moveit-plugins ros-kinetic-moveit-ros ros-kinetic-moveit-resources ros-kinetic-cmake-modules ros-kinetic-usb-cam  ros-kinetic-rviz-visual-tools
 sudo apt-get -f install ros-kinetic-joy
 sudo apt-get -f install ros-kinetic-urdfdom-py ros-kinetic-kdl-parser-py
@@ -28,6 +31,8 @@ rosws merge https://raw.githubusercontent.com/akihikoy/ay_common/master/ay_ros/a
 rosws update
 rospack profile
 source ~/.bashrc
+#TODO: Add the CMAKELIST to the folder.
+
 rosmake ay_util
 rosmake ay_vision
 rosmake ay_sim
