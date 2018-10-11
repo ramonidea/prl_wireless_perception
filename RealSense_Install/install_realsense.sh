54a4c36a9365c14a3ed0385053da57e36273f784
Ramon Qu <quramon@gmail.com>
2018-10-11 13:52:48 -0700

    add realasense ROS wrapper in

--- RealSense_Install/install_realsense.sh
+++ RealSense_Install/install_realsense.sh
@@ -2,6 +2,8 @@
 
 #Use in Ubuntu 16 LTS
 cd ~
+
+#Need to install Cmake 3.12
 curl -OL https://cmake.org/files/v3.12/cmake-3.12.0-rc3-Linux-x86_64.tar.gz
 tar -xvf cmake-3.12.0-rc3-Linux-x86_64.tar.gz
 cd cmake-3.12.0-rc3-Linux-x86_64
@@ -13,9 +15,11 @@ cd ../
 sudo rm -r cmake-3.12.0-rc3-Linux-x86_64
 sudo rm cmake-3.12.0-rc3-Linux-x86_64.tar.gz
 
+#Start installing Realsense Driver, (you can also check librealsen on github)
 mkdir Realsense && cd Realsense
 git clone https://github.com/IntelRealSense/librealsense.git
 cd librealsense
+#We are using an old version of realsense driver.
 git checkout tags/v2.10.0
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
 sudo apt-get install software-properties-common
@@ -34,5 +38,21 @@ cd ~/Realsense/librealsense/
 mkdir build && cd build
 cmake ../ -DBUILD_EXAMPLES=true -DBUILD_PYTHON_BINDINGS=bool:true
 sudo make uninstall && make clean && make -j4 && sudo make install
+
+#Start installing Realsense ROS Wrapper
+mkdir catkin_ws && cd catkin_ws
+mkdir src && cd src
+git clone https://github.com/intel-ros/realsense
+cd realsense
+git checkout tags/2.0.3
+cd ../
+catkin_init_workspace 
+cd ..
+catkin_make clean
+catkin_make -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
+catkin_make install
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
 
 
